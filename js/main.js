1fbfd2424bdb59741647e4ec35425af51d8bd0a2
RasulovAbdurauf <abduraufrasuluzb001@gmail.com>
2024-08-13 13:52:43 +0500

    JS 1-dars
    Zeromax IT-Center, Web programming 36-group

---
+++ js/main.js
@@ -0,0 +1,37 @@
+// JSga kirish
+// console.log("Men faqat console oynasida ko'rinaman!");
+// alert("Saytimizga xush kelibsiz!");
+// document.write("Man bodyga ko'rinaman");
+
+// JS o'zgaruvchilari
+
+// let x = 5;
+// let y = 7;
+// let z = 10;
+// x = 15;
+// let salom = "Assalomu alaykum !";
+// let rang = "#f1f1f1";
+// let x = 15, y = 7, z = 10;
+// document.write(x+y+z);
+
+// JSda ma'lumotlar turlari
+// let son = 45;                   // number
+// let matn = "JavaScript";        // string
+// document.write(typeof son)
+
+// let abdurauf = 13;
+// let ibrohim = 13;
+// let boston = 16;
+
+// document.write(13+13+16);
+
+// JSda matematika
+let x = 5;
+// x += 5; x=5+5            qo'shish
+// x -= 5; x=5-5            ayirish
+// x *= 5; x=5*5            ko'paytirish
+// x /= 5; x=5/5            bo'lish
+// let qoldiq = x%5;        qoldiq chiqarish
+let daraja = x**3;
+
+document.write(daraja);
